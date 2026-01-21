375401fe4e798c1a2cd98d92b6672c5bf7cba25b
lilniggafromdeo'block <ifranco@ctsformacion.com>
2026-01-21 12:25:18 +0100

    v1.7

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+public AudioClip gameMusic;
+
+private AudioSource audioSource;
+
+void Awake () 
+{
+    _audioSource = GetComponent<AudioSource>();
+}
+
+void Start ()
+{
+    StartBGM(); 
+}
+
+void StartBGM() 
+{
+    _audioSource.loop = true
+    _audioSource.clip = gameMusic;
+    _audioSource.Play();
+
+    //_audioSource.Pause(); 
+    //_audioSource.Stop();
+}
+}

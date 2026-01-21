51f12075f80193daf31e2886b7e1eb05358c73b9
lilniggafromdeo'block <ifranco@ctsformacion.com>
2026-01-21 13:13:42 +0100

    v1.9

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   
+   public int killedEnemies = 0;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    public void AddKill ()
+    {
+        killedEnemies++;
+    }
+}
